0a985481e31f744d5b524e5eeb1207b7ffd2fcf3
Rohith <Rohith@rohith007>
2023-09-29 10:55:29 +0530

    first commit

---
+++ Marker/src/FullStack.java
@@ -0,0 +1,18 @@
+
+public class FullStack {
+
+	public static void main(String[] args) {
+		char a='A';
+		int i=203;
+		byte k=4;
+		double d=4.35;
+		float f=4.35f;
+		System.out.println(a);
+		System.out.println(i);
+		System.out.println(k);
+		System.out.println(d);
+		System.out.println(f);
+
+	}
+
+}
